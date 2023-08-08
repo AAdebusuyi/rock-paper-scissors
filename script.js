b5b0192840b24983e044777287a6bd6d8443965d
AAdebusuyi <adeoladonald7@gmail.com>
2023-08-08 12:50:38 +0100

    function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello and welcome to my editor");
+/* 
+console based Rock, Paper, Scissors Game.
+1. Enter a move.
+2. get computer's move.
+3. if we play same move, it's a draw.
+4. else compare moves.
+5. Rock beats sccisors.
+6. Scissors beats paper.
+7. Paper beats rock.
+8. include option to play another round.
+9. else quit and come back next time.
+*/
+
+// let userChoice = prompt("Enter a move: ");
+function getComputerChoice() {
+  let choices = ["Rock", "Paper", "Scissors"];
+  let choicesIndex = Math.floor(Math.random() * choices.length);
+  console.log(choices[choicesIndex]);
+}
+getComputerChoice();
