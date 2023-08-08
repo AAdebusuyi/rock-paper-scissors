40667e6b15a2e1b682bcd75e41951603b95b05a2
AAdebusuyi <adeoladonald7@gmail.com>
2023-08-08 15:08:59 +0100

    added scoring mechanism

--- script.js
+++ script.js
@@ -17,10 +17,15 @@ console based Rock, Paper, Scissors Game.
 // playRound(userChoice, getComputerChoice());
 
 //newer version
+let myScore = 0;
+let computerScore = 0;
+
 for (let i = 0; i < 5; i++) {
   let userChoice = prompt("Enter a move: ").toLowerCase();
   console.log("you played: " + userChoice);
   playRound(userChoice, getComputerChoice());
+  console.log("You scored: " + myScore);
+  console.log("Computer scored: " + computerScore);
 }
 
 function getComputerChoice() {
@@ -40,16 +45,22 @@ function playRound(userSelection, computerSelection) {
     console.log("It's a tie!");
   } else if (userSelection == "rock" && computerSelection == "scissors") {
     console.log("You win! Rock smashes scissors.");
+    myScore++;
   } else if (userSelection == "paper" && computerSelection == "rock") {
     console.log("You win! Paper covers rock.");
+    myScore++;
   } else if (userSelection == "scissors" && computerSelection == "paper") {
     console.log("You win! Scissors dices paper.");
+    myScore++;
   } else if (userSelection == "scissors" && computerSelection == "rock") {
     console.log("You lose! Rock smashes scissors.");
+    computerScore++;
   } else if (userSelection == "rock" && computerSelection == "paper") {
     console.log("You lose! Paper covers rock.");
+    computerScore++;
   } else if (userSelection == "paper" && computerSelection == "scissors") {
     console.log("You lose! Scissors dices paper.");
+    computerScore++;
   } else {
     console.log("something is wrong");
   }
